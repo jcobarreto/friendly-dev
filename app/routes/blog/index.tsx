8d996b64555d06e78d74e0f0ae98b9c09e0a9aeb
Julio Barreto <julio.barreto@gmail.com>
2025-09-16 16:53:02 -0300

    Add routes pages

---
+++ app/routes/blog/index.tsx
@@ -0,0 +1,9 @@
+const BlogPage = () => {
+  return (
+    <section className="text-3xl text-white text-bold mb-8">
+      📝 Blog
+    </section>
+  );
+};
+
+export default BlogPage;
---
+++ app/routes/contact/index.tsx
@@ -0,0 +1,11 @@
+function ContactPage() {
+  return (
+    <section>
+      <h2 className="text-3xl text-bold text-white mb-8 text-center">
+        📬 Contact Me
+      </h2>
+    </section>
+  );
+}
+
+export default ContactPage;
